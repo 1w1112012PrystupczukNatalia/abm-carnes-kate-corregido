a9b883113457ff3fa1f76871ed5408b130273720
1w1112012PrystupczukNatalia <69583450+1w1112012PrystupczukNatalia@users.noreply.github.com>
2020-11-08 19:08:35 -0300

    abm corregido

---
+++ abm carness corregido/Productos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abm_carness
+{
+    class Productos
+    {
+        int codigo;
+        double precio;
+        int stock;
+        int tipo;
+        
+
+
+
+        public int pCodigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+
+
+
+        public double pPrecio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+        public int pStock
+        {
+            get { return stock; }
+            set { stock = value; }
+        }
+
+
+        public int pTipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+        int marca;
+
+        
+
+        override public string ToString()
+        {
+            return codigo + " - " + precio;
+        }
+
+    }
+}
+    
+
